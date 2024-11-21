b2acbc8dd943f3712448fd4cead87e645afac2cb
Mark Santoro <mark_santoro@comcast.net>
2024-11-21 11:05:34 -0800

    build the basic UI

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmout = 8.0
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        Stepper("\(sleepAmout.formatted()) hours", value: $sleepAmout, in:4...12,step: 0.25)
+        NavigationStack{
+            VStack{
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden() // label is hidden for display but needed for voice feature
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in:
+                            4...12, step:0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper("\(coffeeAmount.formatted()) cup(s)", value: $coffeeAmount, in:
+                            1...20)
+                
+            }
+            .navigationTitle("Better Rest")
+            .toolbar{
+                Button("Calculate", action: calculateBedTime)
+            }
+        }
+        
+ 
     }
+    
+    func calculateBedTime() {
+        
+    }
+
+     
+
 }
 
 #Preview {
